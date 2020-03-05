8c04d76e041c8e49d6e9a1219abba5000c7515c8
Pius Shungu <piusshungu@Graysons-MacBook-Pro-2.local>
2020-03-05 11:40:57 +0300

    add flash messages class

--- app/Orders.php
+++ app/Orders.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Emadadly\LaravelUuid\Uuids;
 
 class Orders extends Model
 {
+    use Uuids;
+
+  
+    /**
+    * Indicates if the IDs are auto-incrementing.
+    *
+    * @var bool
+    */
+    public $incrementing = false;
+  
+    
     //
 }
